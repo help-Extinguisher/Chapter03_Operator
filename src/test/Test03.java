73ea50dfdad37e0972940b2518a938c6e1120ebe
HOME <HOME@ManaHerb>
2021-07-30 07:07:53 +0900

    210730

--- src/test/Test03.java
+++ src/test/Test03.java
@@ -2,7 +2,6 @@ package test;
 
 public class Test03 {
 
-	public static void main(String[] args) {
 		
 //		while문과 Math.radom()메소드를 이용해서 2개의 주사위를 던졌을 때
 //		나오는 눈을 (눈1, 눈2)형태로 출력하고, 눈의 합이 5가 아니면 계속 주사위를 던지고,
@@ -29,13 +28,35 @@ public class Test03 {
 			   
 //---------------------------------------------------------------------------------------------------
 		
+	public static void main(String[] args) {
 		
 		
-			   
-			   
-			   }
+				
+		 while (true) {
+			 int a = (int) (Math.random() * 6 + 1);
+			 int b = (int) (Math.random() * 6 + 1);
+			 int c = a+b;
+			 
+			 if (c != 5) {
+				 System.out.println("(" + a + " , " + b + ")");
+				 System.out.println("주사위의 합은 " + (a+b) + "입니다."); 
+		 	 }
+		 
+			 if (c == 5) {
+				 System.out.println("(" + a + " , " + b + ")");
+				 System.out.println("눈의 합이 5가 나왔으므로 실행을 멈췄습니다.");
+				 break;
+			 }
+		 }			   
+	}
+}
+
+
+
+
 			
 
-	}
+
+
 
 
